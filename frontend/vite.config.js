02c24ca2037a95878b95efef6529860a8f89a7b3
Dang Hoang <danghai.hoang.work@gmail.com>
2026-06-28 02:36:07 +0700

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/medparcours-ai/', // ĐỔI TÊN Ở DÒNG NÀY
-  server: { port: 5173 },
-})
